e1d59bcbcae9bc285583ddce3f694091b2dd425b
katie424 <7080katie@gmail.com>
2025-02-08 22:55:26 +0900

    ui: 패딩 수정

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
       <img src={yongyong} alt="yongyong" className="w-1/2" />
       <div className="text-lg">원하는 말투로 바꿔주는</div>
       <img src={title} alt="title" className="w-3/4 mt-2" />
-      <div className="flex flex-col justify-center items-center px-10 w-full gap-4">
+      <div className="flex flex-col justify-center items-center px-10 w-full gap-4 pb-10">
         <button className="bg-kakao text-black px-4 py-2 mt-32 w-full h-11 rounded-lg">
           카카오로 시작하기
         </button>
